e2fffe29ecdc221da3279362de68a96b0ddd6a0f
Anushka Dasgupta <mithis2000@gmail.com>
2025-04-29 12:29:55 +0530

    [Setup] Initialize Vite project with Tailwindcss, Vanilla Javascript and Vitest

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './pages/**/*.{html,js}',
+    './components/**/*.{html,js}',
+    "./index.html", 
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./public/**/*.{html,js}",
+    "./src/**/*.{html,js}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
